38f2fc69c4327e9322cbe672fecb27ad9a88275d
ravi <ravipujara1999@gmail.com>
2023-07-04 11:40:36 +0530

    Init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'auth',
+    pathMatch:'full'
+  },
+  {
+    path:'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path:'dashboard',
+    loadChildren:()=>import('./modules/dashboard/dashboard.module').then((d)=>d.DashboardModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path:'sprint',
+    loadChildren:()=>import('./modules/sprint/sprint.module').then((s)=>s.SprintModule),
+    canActivate: [AuthGuard]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
